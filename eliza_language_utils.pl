54cd0a2e6ea72af81afbab62f37cba5048733616
František Trebuňa <ferotre@gmail.com>
2020-04-27 16:18:42 +0200

    Add grammatical cases detection

--- eliza_language_utils.pl
+++ eliza_language_utils.pl
@@ -54,6 +54,9 @@ conditional_lemm([s,e,s,t,e,r,n,i,c|_], [f,a,m,i,l,y]) :- !.
 conditional_lemm([b,r,a,t,r,a,n|_], [f,a,m,i,l,y]) :- !.
 conditional_lemm([s,t,r,y,k|_], [f,a,m,i,l,y]) :- !.
 conditional_lemm([t,e,t|_], [f,a,m,i,l,y]) :- !.
+conditional_lemm([d,e,d|_], [f,a,m,i,l,y]) :-!.
+conditional_lemm([t,a,t,k|_], [f,a,m,i,l,y]) :-!.
+conditional_lemm([b,a,b,k|_], [f,a,m,i,l,y]) :- !.
 conditional_lemm(X,X).
 % is_declination(+X, +Y)
 %   returns true if X is declination of Y
@@ -73,26 +76,117 @@ member_declined(Word, [Input_Head|_]) :-
 
 member_declined(Word, [_| Input_rest]) :-
     member_declined(Word, Input_rest).
+% ----
+% --- noun matching ---
+
+hard(h).
+hard(k).
+hard(g).
+hard(d).
+hard(t).
+hard(n).
+hard(l).
+
+hard(b).
+hard(p).
+hard(m).
+hard(r).
+hard(s).
+hard(v).
+hard(z).
+
+soft(c).
+soft(j).
+
+gram_case_feminine_sg(X, n) :-
+    append(_, [a], X), !.
+
+gram_case_feminine_sg(X,g) :-
+    append(_, [Conson, Y], X), 
+    (
+        hard(Conson) -> Y = y;
+        soft(Conson) -> Y = e;
+        fail
+    ), !.
+
+gram_case_feminine_sg(X,d) :-
+    append(_, [Conson, Y], X), 
+    (
+        hard(Conson) -> Y = e;
+        soft(Conson) -> Y = i;
+        fail
+    ).
+
+gram_case_feminine_sg(X,a) :-
+    append(_, [u], X), !.
+
+gram_case_feminine_sg(X,l) :-
+    append(_, [Conson, Y], X), 
+    (
+        hard(Conson) -> Y = e;
+        soft(Conson) -> Y = i;
+        fail
+    ), !.
+
+gram_case_feminine_sg(X,i) :-
+    append(_, [o,u], X), !.
+
+
+gram_case_masculine_sg(X, ga) :-
+    append(_,[a], X),!.
+
+gram_case_masculine_sg(X, dl) :-
+    append(_,[o,v,i], X),!.
+
+gram_case_masculine_sg(X, i) :-
+    append(_,[o,m], X),!.
+
+gram_case_masculine_sg(X,n) :-
+    append(_,[Conson], X),
+    (soft(Conson);hard(Conson)),!.
+
+gram_case_masculine_sg([d,e,d,o],n) :- !.
+gram_case_masculine_sg([t,a,t,k,o],n) :- !.
+
+% ---
+% --- possessive matching ---
+possessive_([m,o,j|_]).
+possessive_([t,v,o,j|_]).
+possessive_([n,a,s|_]).
+possessive_([v,a,s|_]).
+possessive_([j,e,h,o]).
+possessive_([j,e,j]).
+possessive_([i,c,h]).
+
+possessive(X) :-
+    atom_chars(X, Chars), 
+    possessive_(Chars).
 
 % force at least 1 char after base of declination
-family_feminine_([m,a,m|[_|_]]).
-family_feminine_([s,e,s,t,r|[_|_]]).
-family_feminine_([t,e,t|[_|_]]).
-family_feminine_([s,e,s,t,e,r,n,i,c|[_|_]]).
+family_feminine_([m,a,m|[_|_]]) :-!.
+family_feminine_([m,a,t,k|[_|_]]) :-!.
+family_feminine_([s,e,s,t,r|[_|_]]) :-!.
+family_feminine_([t,e,t|[_|_]]) :-!.
+family_feminine_([s,e,s,t,e,r,n,i,c|[_|_]]) :-!.
+family_feminine_([b,a,b,k|[_|_]]) :-!.
 
-family_masculine_([b,r,a,t|[_|_]]).
-family_masculine_([o,t,c|[_|_]]).
-family_masculine_([o,t,e,c]).
-family_masculine_([s,t,r,y,k|[_|_]]).
-family_masculine_([b,r,a,t,r,a,n|[_|_]]).
+family_masculine_([b,r,a,t|[_|_]]) :-!.
+family_masculine_([o,t,c|[_|_]]) :-!.
+family_masculine_([o,t,e,c]) :-!.
+family_masculine_([t,a,t,k|[_|_]]) :-!.
+family_masculine_([s,t,r,y,k|[_|_]]) :-!.
+family_masculine_([b,r,a,t,r,a,n|[_|_]]) :-!.
+family_masculine_([d,e,d|[_|_]]) :-!.
 
-family_feminine(X) :-
+family_feminine(X, Gram_case) :-
     atom_chars(X, Chars),
-    family_feminine_(Chars).
+    family_feminine_(Chars),
+    gram_case_feminine_sg(Chars, Gram_case).
 
-family_masculine(X) :-
+family_masculine(X, Gram_case) :-
     atom_chars(X, Chars),
-    family_masculine_(Chars).
+    family_masculine_(Chars),
+    gram_case_masculine_sg(Chars, Gram_case).
 
 everybody(vsetci).
 everybody(nikto).
